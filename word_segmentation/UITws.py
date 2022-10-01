88dea2edc71077a15171bbefb83dc2fc2ccb379d
lam061197 <sonlam06111998@gmail.com>
2022-10-01 16:31:29 +0700

    Create UITws.py

---
+++ word_segmentation/UITws.py
@@ -0,0 +1,6 @@
+from UITws_v1 import UITws_v1
+
+def word_segment(texts):
+    model = UITws_v1('base_sep_sfx.pkl')
+    segmented_texts = model.segment(texts=texts, pre_tokenized=True, batch_size=256)
+    return segmented_texts
