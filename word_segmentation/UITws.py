71dc93b89dff501454e75ff0e73aa9450d13bf62
lam061197 <sonlam06111998@gmail.com>
2022-10-01 18:32:14 +0700

    update

--- word_segmentation/UITws.py
+++ word_segmentation/UITws.py
@@ -1,10 +1,11 @@
 from word_segmentation.UITws_v1 import UITws_v1
+from nltk import tokenize
 
 def word_segment(texts, single_text=False):
     model = UITws_v1('word_segmentation/base_sep_sfx.pkl')
     if single_text==False:
         segmented_texts = model.segment(texts=texts, pre_tokenized=True)
     else:
-        texts = nltk.sent_tokenize(texts)
+        texts = tokenize.sent_tokenize(texts)
         segmented_texts = model.segment(texts=texts, pre_tokenized=True)
     return segmented_texts
