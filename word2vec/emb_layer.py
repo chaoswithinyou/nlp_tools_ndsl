b9d256ef47f55a4af9843eac6c55d0a31a1d99a8
lam061197 <sonlam06111998@gmail.com>
2022-10-03 00:54:39 +0700

    update

--- word2vec/emb_layer.py
+++ word2vec/emb_layer.py
@@ -1,23 +1,5 @@
 import torch
 import torch.nn as nn
-from tqdm import tqdm
-from nltk import tokenize
-
-def word2ids(sen_list, word2idx, maxlen, vector_size):
-    x = torch.zeros((len(sen_list),1,maxlen))
-    for i in tqdm(range(len(sen_list))):
-      doc = tokenize.word_tokenize(sen_list[i])
-      fil_doc_w2index = []
-      for word in doc:
-        try:
-          fil_doc_w2index.append(word2idx[word])
-        except Exception:
-          pass
-      if len(fil_doc_w2index)<=maxlen:
-        x[i] = torch.cat((torch.LongTensor(fil_doc_w2index),torch.zeros(maxlen-len(fil_doc_w2index)))).unsqueeze(0)
-      else:
-        x[i] = torch.LongTensor(fil_doc_w2index[-maxlen-1:-1]).unsqueeze(0)
-    return x
 
 def create_emb_layer_from_vector_mat(weights_matrix, non_trainable=False):
     weights_matrix = torch.Tensor(weights_matrix)
